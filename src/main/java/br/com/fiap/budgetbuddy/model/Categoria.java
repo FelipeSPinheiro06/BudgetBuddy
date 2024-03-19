e155e39e4fba1df9efff1ea6018c381e32877d11
FelipePinheiro <fsp12371@git.com>
2024-03-18 22:47:24 -0300

    Refatoração de Código

--- src/main/java/br/com/fiap/budgetbuddy/model/Categoria.java
+++ src/main/java/br/com/fiap/budgetbuddy/model/Categoria.java
@@ -1,52 +1,21 @@
 package br.com.fiap.budgetbuddy.model;
 
-import java.util.Random;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 // Imutável
 // Substitui a classe clichê model com get e set
 @Entity
+@Data
 public class Categoria {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String icone;
-    
-    public Categoria(Long id, String nome, String icone) {
-        this.id = id;
-        this.nome = nome;
-        this.icone = icone;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getIcone() {
-        return icone;
-    }
-
-    public void setIcone(String icone) {
-        this.icone = icone;
-    }
-
     
 
 }
